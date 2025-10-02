7fc4def916f8d55a688ecae282d14248c7aff17c
sfdhd100 <sfdhd100@gmail.com>
2025-10-02 22:22:04 +0900

    Add files via upload

--- AI/sentiment_server.py
+++ AI/sentiment_server.py
@@ -8,7 +8,7 @@ import json
 import threading
 
 HOST = '127.0.0.1'
-PORT = 5000
+PORT = 8000
 
 class SentimentPredictor:
     def __init__(self):
@@ -29,38 +29,40 @@ class SentimentPredictor:
         self.model.eval()  # 모델을 평가 모드로 전환
         print(f"Using device: {self.device}")
 
-    def predict(self, comments):
+    def predict(self, texts):
         """
         댓글 리스트를 입력받아 감성 라벨을 반환합니다.
         """
         print("분석 시작")
-        if not isinstance(comments, list):
-            comments = [comments]  # 단일 문자열도 리스트로 변환
+        if not isinstance(texts, list):
+            texts = [texts]  # 단일 문자열도 리스트로 변환
 
         # 토큰화 및 GPU 이동
-        inputs = self.tokenizer(comments, padding=True, truncation=True, return_tensors="pt")
+        inputs = self.tokenizer(texts, padding=True, truncation=True, return_tensors="pt")
         inputs = {k: v.to(self.device) for k, v in inputs.items()}
 
         # 모델 예측
         with torch.no_grad():
             outputs = self.model(**inputs)
-            predictions = outputs.logits.argmax(dim=-1)
+            predicted_labels_tensor = outputs.logits.argmax(dim=-1)
 
-        # 라벨 매핑
-        results = [[comment, pred.item()] for comment, pred in zip(comments, predictions)]
-        return results
+        return predicted_labels_tensor.tolist()
 
 def sentiment(conn, addr):
     print(f"클라이언트가 연결되었습니다.: {addr}")
     try:
         while True:
             with conn.makefile("r", encoding="utf-8") as f:
-                data = f.readline().strip()
-                comments = json.loads(data)
+                json_string = f.readline().strip()
+                request_body = json.loads(json_string)
+                comment_objects = request_body['comments']
+                comment_texts = [comment['text'] for comment in comment_objects]
             
-                results = predictor.predict(comments)
-                json_results = json.dumps(results, ensure_ascii=False)
-                conn.sendall((json_results + "\n").encode("utf-8"))
+                results = predictor.predict(comment_texts)
+                for comment_dict, label in zip(comment_objects, results):
+                    comment_dict['prediction'] = label
+                response_json = json.dumps(request_body, ensure_ascii=False)
+                conn.sendall((response_json + "\n").encode("utf-8"))
 
     finally:
         print(f"클라이언트와의 연결을 종료합니다: {addr}")
